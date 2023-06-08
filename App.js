064321af49b7a0081dac5b714617fb9ebf575c30
ragss8 <96374035+ragss8@users.noreply.github.com>
2023-06-08 08:54:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function MyButton() {
+  return (
+    <button>
+      This is a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my First React-app</h1>
+      <MyButton />
+    </div>
+  );
+}
